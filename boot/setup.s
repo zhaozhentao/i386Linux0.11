ce48a577857bb6c3c0fa81736b2a238afb86e9e3
zhaozhentao <qq344696734@gmail.com>
2023-03-14 10:30:50 +0800

    结束setup

--- boot/setup.s
+++ boot/setup.s
@@ -113,21 +113,67 @@ end_move:
   lidt idt_48                               # 加载中断描述符
   lgdt gdt_48                               # 加载全局描述符表
 
-gdt:
-  .word 0,0,0,0                             # dummy
+# 开启 A20
+  inb  $0x92, %al                           # open A20 line(Fast Gate A20).
+  orb  $0b00000010, %al
+  outb %al, $0x92
 
+# 重新对中断编程
+  mov  $0x11, %al                           # initialization sequence(ICW1)
+                                            # ICW4 needed(1),CASCADE mode,Level-triggered
+  out  %al, $0x20                           # send it to 8259A-1
+  .word	0x00eb,0x00eb                       # jmp $+2, jmp $+2
+  out	%al, $0xA0                          # and to 8259A-2
+  .word	0x00eb,0x00eb
+  mov	$0x20, %al                          # start of hardware int's (0x20)(ICW2)
+  out	%al, $0x21                          # from 0x20-0x27
+  .word	0x00eb,0x00eb
+  mov	$0x28, %al                          # start of hardware int's 2 (0x28)
+  out	%al, $0xA1                          # from 0x28-0x2F
+  .word	0x00eb,0x00eb                       # IR 7654 3210
+  mov	$0x04, %al                          # 8259-1 is master(0000 0100) --\
+  out	%al, $0x21                          #				|
+  .word	0x00eb,0x00eb                       #			 INT	/
+  mov	$0x02, %al                          # 8259-2 is slave(       010 --> 2)
+  out	%al, $0xA1
+  .word	0x00eb,0x00eb
+  mov	$0x01, %al                          # 8086 mode for both
+  out	%al, $0x21
+  .word	0x00eb,0x00eb
+  out	%al, $0xA1
+  .word	0x00eb,0x00eb
+  mov	$0xFF, %al                          # mask off all interrupts for now
+  out	%al, $0x21
+  .word	0x00eb,0x00eb
+  out	%al, $0xA1
+
+# 设置好中断、全局描述符表之后，正式进入保护模式
+  mov  %cr0, %eax                           # 获取机器状态 get machine status(cr0|MSW)	
+  bts  $0, %eax                             # 设置保护模式的位
+  mov  %eax, %cr0                           # 开启保护模式
+              
+# segment-descriptor        (INDEX:TI:RPL)
+  .equ sel_cs0, 0x0008                      # 选择 gdt 描述符
+  ljmp $sel_cs0, $0                         # 选择下面的 gdt 表第一个描述符，因为段选择子是 0x8, 偏移量是0,离开 setup 模块
+
+gdt:
+  .word 0,0,0,0                             # 第一个描述符，不用
+# 在 gdt 表中这里的偏移量是 0x8,是内核代码段选择服的值，对应上面的 ljmp $sel_cs0, $0
   .word 0x07FF                              # 8Mb - limit=2047 (2048*4096=8Mb)
   .word 0x0000                              # base address=0
   .word 0x9A00                              # code read/exec
   .word 0x00C0                              # granularity=4096, 386
-
+# 在 gdt 表中这里的偏移量是 0x10 是内核数据段选择符的值
   .word 0x07FF                              # 8Mb - limit=2047 (2048*4096=8Mb)
   .word 0x0000                              # base address=0
   .word 0x9200                              # data read/write
   .word 0x00C0                              # granularity=4096, 386
+
+# CPU 进入保护模式之前要求设置 IDT 表，这里先设置一个空表
 idt_48:
-  .word 0                                   # idt limit=0
-  .word 0, 0                                # idt base=0L
+  .word 0                                   # 这里两个字节是 idt 表的限长,这里先设 为 0
+  .word 0, 0                                # idt 表在线性空间地址中 32 位基地址，这里设置为 0
+
 gdt_48:
   .word 0x800                               # gdt limit=2048, 256 GDT entries
   .word 512+gdt, 0x9                        # gdt base = 0X9xxxx
@@ -136,7 +182,4 @@ msg2:
   .byte 13, 10
   .ascii "Now we are in setup ..."
   .byte 13, 10, 13, 10
-
-cur:
-  .ascii "Cursor POS:"
 
