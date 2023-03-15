1a6429820cf52108cc668a05139b0b641d042d0e
zhaozhentao <qq344696734@gmail.com>
2023-03-15 17:56:30 +0800

    保存主内存开始位置

--- init/main.c
+++ init/main.c
@@ -3,12 +3,13 @@
 /*
  * 这些参数是在 setup 模块里面存下来的
  */
-#define EXT_MEM_K (*(unsigned short *)0x90002)
+#define EXT_MEM_K (*(unsigned short *)0x90002)             // 可以看到是 14M 内存
 #define DRIVE_INFO (*(struct drive_info *)0x90080)
 #define ORIG_ROOT_DEV (*(unsigned short *)0x901FC)
 
 static long memory_end = 0;
 static long buffer_memory_end = 0;
+static long main_memory_start = 0;
 
 struct drive_info { char dummy[32]; } drive_info;
 
@@ -18,8 +19,10 @@ void main(void)
     drive_info = DRIVE_INFO;
     memory_end = (1 << 20) + (EXT_MEM_K << 10);
     memory_end &= 0xfffff000;
+
     if (memory_end > 16*1024*1024)
         memory_end = 16*1024*1024;
+
     if (memory_end > 12*1024*1024) 
         buffer_memory_end = 4*1024*1024;
     else if (memory_end > 6*1024*1024)
@@ -27,5 +30,6 @@ void main(void)
     else
         buffer_memory_end = 1*1024*1024;
 
+    main_memory_start = buffer_memory_end;
 }
 
