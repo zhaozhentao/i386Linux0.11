fbf9d8d0d61fd703a6830bf115474fb16c4d93db
zhaozhentao <qq344696734@gmail.com>
2023-03-15 17:41:17 +0800

    加载 drive_info

--- init/main.c
+++ init/main.c
@@ -1,9 +1,13 @@
 #include <linux/fs.h>
 
+#define DRIVE_INFO (*(struct drive_info *)0x90080)
 #define ORIG_ROOT_DEV (*(unsigned short *)0x901FC)
+
+struct drive_info { char dummy[32]; } drive_info;
 
 void main(void)
 {
     ROOT_DEV = ORIG_ROOT_DEV;
+    drive_info = DRIVE_INFO;
 }
 
