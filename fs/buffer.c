32f6b53068120f29593fae0d58c25f888365ddc0
zhaozhentao <qq344696734@gmail.com>
2023-03-22 17:40:47 +0800

    end 说明

--- fs/buffer.c
+++ fs/buffer.c
@@ -1,4 +1,4 @@
-extern int end;
+extern int end;                   // 这个 end 符号是由编译器生成的，整个 system 模块的最后一个地址，描述不准确，可以查看 .map 文件找这个标号
 
 struct buffer_head * start_buffer = (struct buffer_head *) &end;
 
