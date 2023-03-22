467dfee1ba183e30e4cc58baa4591dc1ba083ef3
zhaozhentao <qq344696734@gmail.com>
2023-03-22 16:38:13 +0800

    init task list

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1,6 +1,7 @@
 #ifndef _SCHED_H
 #define _SCHED_H
 
+#define NR_TASKS 64
 #define HZ 100
 
 #include <linux/head.h>
--- kernel/sched.c
+++ kernel/sched.c
@@ -3,6 +3,15 @@
 
 long startup_time=0;
 
+union task_union {
+    struct task_struct task;
+    char stack[PAGE_SIZE];
+};
+
+static union task_union init_task = {INIT_TASK,};
+
+struct task_struct * task[NR_TASKS] = {&(init_task.task), };
+
 long user_stack [ PAGE_SIZE>>2 ] ;
 
 // 这里 0x10 是内核段选择符
@@ -10,13 +19,6 @@ struct {
   long * a;
   short b;
 } stack_start = { & user_stack [PAGE_SIZE>>2] , 0x10 };
-
-union task_union {
-    struct task_struct task;
-    char stack[PAGE_SIZE];
-};
-
-static union task_union init_task = {INIT_TASK,};
 
 // todo: this method
 void math_state_restore() {
@@ -29,5 +31,14 @@ void sched_init(void) {
 
     set_tss_desc(gdt+FIRST_TSS_ENTRY,&(init_task.task.tss));
     set_ldt_desc(gdt+FIRST_LDT_ENTRY,&(init_task.task.ldt));
+    p = gdt+2+FIRST_TSS_ENTRY;
+    for(i=1;i<NR_TASKS;i++) {
+        task[i] = NULL;
+        p->a=p->b=0;
+        p++;
+        p->a=p->b=0;
+        p++;
+    }
+
 }
 
