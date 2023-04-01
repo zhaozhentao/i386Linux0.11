a39f028bacd9152c8bf021b1edce57955dd03be1
zhaozhentao <qq344696734@gmail.com>
2023-04-01 16:09:29 +0800

    goto head.s

--- Makefile
+++ Makefile
@@ -1,13 +1,27 @@
+include Makefile.header
+
+LDFLAGS	+= -Ttext 0 -e startup_32
+
 all: Image
 
-Image: boot/bootsect boot/setup
-	tools/build.sh boot/bootsect boot/setup Image
+Image: boot/bootsect boot/setup tools/system
+	cp -f tools/system system.tmp
+	$(STRIP) system.tmp
+	$(OBJCOPY) -O binary -R .note -R .comment system.tmp tools/kernel
+	tools/build.sh boot/bootsect boot/setup tools/kernel Image
 
 boot/bootsect: boot/bootsect.s
 	make bootsect -C boot
 
 boot/setup: boot/setup.s
 	make setup -C boot
+
+boot/head.o: boot/head.s
+	make head.o -C boot
+
+tools/system: boot/head.o
+	$(LD) -g $(LDFLAGS) boot/head.o \
+	-o tools/system
 
 start: Image
 	qemu-system-i386 -m 16M -boot a -fda Image -curses
--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,10 @@
 
 bootsect=$1
 setup=$2
-IMAGE=$3
+system=$3
+IMAGE=$4
+
+SYS_SIZE=$((0x3000*16))
 
 # Write bootsect (512 bytes, one sector) to stdout
 [ ! -f "$bootsect" ] && echo "找不到二进制文件 bootsect" && exit -1
@@ -11,4 +14,10 @@ dd if=$bootsect bs=512 count=1 of=$IMAGE 2>&1 >/dev/null
 # Write setup(4 * 512bytes, four sectors) to stdout
 [ ! -f "$setup" ] && echo "找不到二进制文件 setup" && exit -1
 dd if=$setup seek=1 bs=512 count=4 of=$IMAGE 2>&1 >/dev/null
+
+# Write system(< SYS_SIZE) to stdout
+[ ! -f "$system" ] && echo "there is no system binary file there" && exit -1
+system_size=`wc -c $system |cut -d" " -f1`
+[ $system_size -gt $SYS_SIZE ] && echo "the system binary is too big" && exit -1
+dd if=$system seek=5 bs=512 count=$((2888-1-4)) of=$IMAGE 2>&1 >/dev/null
 
