25432fbb8e7a7a13e5009b9d592772e761fec134
zhaozhentao <qq344696734@gmail.com>
2023-10-05 17:29:31 +0800

    释放页表

--- mm/memory.c
+++ mm/memory.c
@@ -1,3 +1,6 @@
+#define invalidate() \
+__asm__("movl %%eax,%%cr3"::"a" (0))
+
 #define LOW_MEM 0x100000
 #define PAGING_MEMORY (15*1024*1024)           // 主内存最多15M
 #define PAGING_PAGES (PAGING_MEMORY>>12)       // 分页后的内存页数
@@ -44,6 +47,41 @@ void free_page(unsigned long addr)
     if (mem_map[addr]--) return;
         mem_map[addr]=0;
     panic("trying to free free page");
+}
+
+int free_page_tables(unsigned long from,unsigned long size)
+{
+    unsigned long *pg_table;
+    unsigned long * dir, nr;
+
+    if (from & 0x3fffff)
+        panic("free_page_tables called with wrong alignment");
+    if (!from)
+        panic("Trying to free up swapper memory space");
+    // 将参数中的 size (字节为单位) 转换为 4M 的整数，即多少个 4M，即页目录项个数
+    // + 0x3fffff 是为了处理进位，如原来为 0.1M，+ 0x3fffff 后 >> 22 size 就是 1
+    size = (size + 0x3fffff) >> 22;
+    // 计算页目录项号 from >> 22 ，因为每个页目录项大小为 4 字节，且页目录起始地址为 0 ，
+    // 所以页目录项号转换为页目录项所在地址(指针) 为页目录项号 * 4 ，
+    // 所以由线性地址 from 中取页目录项地址转换为 from >> 22 << 2 = from >> 20
+    dir = (unsigned long *) ((from>>20) & 0xffc); /* _pg_dir = 0 */
+    // 循环将一个页表里面的每个项清零
+    for ( ; size-->0 ; dir++) {
+        if (!(1 & *dir))
+            continue;
+        pg_table = (unsigned long *) (0xfffff000 & *dir);
+        for (nr=0 ; nr<1024 ; nr++) {
+            if (1 & *pg_table)
+                free_page(0xfffff000 & *pg_table);
+            *pg_table = 0;
+            pg_table++;
+        }
+        free_page(0xfffff000 & *dir);
+        // 将页目录项清零
+        *dir = 0;
+    }
+    invalidate();
+    return 0;
 }
 
 void mem_init(long start_mem, long end_mem)
