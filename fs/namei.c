4193aa02535cb3a666908300b1c283e413d7a3d1
zhaozhentao <qq344696734@gmail.com>
2023-11-21 22:26:53 +0800

    完善 find_entry

--- fs/namei.c
+++ fs/namei.c
@@ -28,19 +28,61 @@ static int permission(struct m_inode * inode,int mask)
     return 0;
 }
 
+static int match(int len,const char * name,struct dir_entry * de)
+{
+    register int same ;
+
+    if (!de || !de->inode || len > NAME_LEN)
+        return 0;
+    if (len < NAME_LEN && de->name[len])
+        return 0;
+    __asm__("cld\n\t"
+        "fs ; repe ; cmpsb\n\t"
+        "setz %%al"
+        :"=a" (same)
+        :"0" (0),"S" ((long) name),"D" ((long) de->name),"c" (len)
+        );
+    return same;
+}
+
 // 从目录中查找指定的目录项，返回目录项所在的缓冲区
 static struct buffer_head * find_entry(struct m_inode ** dir,
     const char * name, int namelen, struct dir_entry ** res_dir) {
-	int entries;
-	int block,i;
-	struct buffer_head * bh;
-	struct dir_entry * de;
-	struct super_block * sb;
+    int entries;
+    int block,i;
+    struct buffer_head * bh;
+    struct dir_entry * de;
+    struct super_block * sb;
+
+#ifdef NO_TRUNCATE
+    if (namelen > NAME_LEN)
+        return NULL;
+#else
+    if (namelen > NAME_LEN)
+        namelen = NAME_LEN;
+#endif
 
     entries = (*dir)->i_size / (sizeof (struct dir_entry));
     *res_dir = NULL;
     if (!namelen)
         return NULL;
+
+    /* check for '..', as we might have to do some "magic" for it */
+    if (namelen==2 && get_fs_byte(name)=='.' && get_fs_byte(name+1)=='.') {
+        /* '..' in a pseudo-root results in a faked '.' (just change namelen) */
+        if ((*dir) == current->root)
+            namelen=1;
+        else if ((*dir)->i_num == ROOT_INO) {
+            /* '..' over a mount-point results in 'dir' being exchanged for the mounted
+            directory-inode. NOTE! We set mounted, so that we can iput the new dir */
+            sb=get_super((*dir)->i_dev);
+            if (sb->s_imount) {
+                iput(*dir);
+                (*dir)=sb->s_imount;
+                (*dir)->i_count++;
+            }
+        }
+    }
 
     if (!(block = (*dir)->i_zone[0]))
         return NULL;
@@ -55,10 +97,16 @@ static struct buffer_head * find_entry(struct m_inode ** dir,
         if ((char *)de >= BLOCK_SIZE+bh->b_data) {
             brelse(bh);
             bh = NULL;
+            if (!(block = bmap(*dir,i/DIR_ENTRIES_PER_BLOCK)) ||
+                !(bh = bread((*dir)->i_dev,block))) {
+                i += DIR_ENTRIES_PER_BLOCK;
+                continue;
+            }
+            de = (struct dir_entry *) bh->b_data;
         }
 
         // 通过文件名对比目录项
-        if (!strncmp(de->name, name, namelen)) {
+        if (match(namelen,name,de)) {
             *res_dir = de;
             return bh;
         }
